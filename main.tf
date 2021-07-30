5ed3cf2eda80b5ba54791f61bb588deb2f770183
Gagandeep Singh <admin.gagan@gmail.com>
2021-07-30 11:22:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "server2" {
   source  = "app.terraform.io/IBM-Techlanders/module-ec2-s3/aws"
   version = "1.0.0"
     i_type = "t2.nano"
-    bucket = "gagandeep-bucket-33"
+    bucket = "gagandeep-bucket-44"
 }
 
 output "IP" {

37010c6994794d3a6eff212f2ebc0f3d34666d98
Gagandeep Singh <admin.gagan@gmail.com>
2021-07-30 11:17:04 +0530

    new file

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "remote" {
+    organization = "IBM-Techlanders"
+
+    workspaces {
+      name = "module-check"
+    }
+  }
+}
+provider "aws" {
+    region = "us-east-2"
+}
+
+module "server2" {
+  source  = "app.terraform.io/IBM-Techlanders/module-ec2-s3/aws"
+  version = "1.0.0"
+    i_type = "t2.nano"
+    bucket = "gagandeep-bucket-33"
+}
+
+output "IP" {
+ value = module.server2.PUBLIC_IP
+}
